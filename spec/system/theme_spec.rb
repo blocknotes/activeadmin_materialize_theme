894b104007686fb637682cb542931e64744c76a8
Mattia Roccoberton <mat@blocknot.es>
2021-05-29 16:42:06 +0200

    Fix RuboCop alerts

--- spec/system/theme_spec.rb
+++ spec/system/theme_spec.rb
@@ -5,10 +5,10 @@ RSpec.describe 'Theme', type: :system do
     visit '/admin/posts'
 
     expect(page).to have_css('body.active_admin .title_bar', style: { 'margin-bottom': '0px' })
-    style = { 'display': 'inline-block', 'margin-bottom': '0px' }
+    style = { display: 'inline-block', 'margin-bottom': '0px' }
     expect(page).to have_css('body.active_admin #titlebar_left h2', style: style)
     expect(page).to have_css('body.active_admin .header', style: { 'margin-bottom': '0px' })
-    expect(page).to have_css('body.active_admin .sidebar h3', style: { 'margin': '0px' })
-    expect(page).to have_css('body.active_admin .footer', style: { 'position': 'absolute' })
+    expect(page).to have_css('body.active_admin .sidebar h3', style: { margin: '0px' })
+    expect(page).to have_css('body.active_admin .footer', style: { position: 'absolute' })
   end
 end
